a6d4f034efdf34010a68b1cd2453a3a6e6061a07
Behtsa <monze_gdbm@hotmail.com>
2017-10-23 15:12:41 -0500

    version 1 arreglos

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var row = [1, 2, 3, 4];
-
-row.push(5);
+var objeto = {perrito: "perrito"};
+row.push("hola");
 //row.shift()
+row.pop();
+row.unshift(objeto.perrito);
 document.write(row);
 console.log(row);
